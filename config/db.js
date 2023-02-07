ff4f30a723f8e35eae38f14356d095915e649913
Kaushik Sriram <kaushik162000@gmail.com>
2023-02-08 00:32:06 +0530

    Add .env file

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const connect = () => {
-  return mongoose.connect(
-    "mongodb+srv://kaushiksriram16:kaushik123@cluster0.ozh7y.mongodb.net/?retryWrites=true&w=majority"
-  );
+  return mongoose.connect(process.env.MONGO_URI);
 };
 module.exports = connect;
